3120db2098bd6acdf203235c9a5e8a7667d81ed4
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-07-20 11:34:04 +0200

    styling chat

--- FRONTger/src/components/homePage/homeComponents/ChatComponent.vue
+++ FRONTger/src/components/homePage/homeComponents/ChatComponent.vue
@@ -7,9 +7,9 @@ const myName = ref("Luke");
 
 <template>
     <div class="chat">
-        <div class="bubble" v-for="mess in exampleTalking">
-            <img :src="mess.imgProfile" alt="" />
-            <div class="message" :class="{ yourMess: myName === mess.name }">
+        <div class="bubble" v-for="mess in exampleTalking" :class="{ yourMess: myName === mess.name }">
+            <img :src="mess.imgProfile" alt="" :class="{ displayPhoto: mess.lastMessageUser }" />
+            <div class="message">
                 {{ mess.message }}
             </div>
         </div>
@@ -17,23 +17,41 @@ const myName = ref("Luke");
 </template>
 <style lang="scss" scoped>
 .chat {
-    margin-top: 65px;
     background-color: rgb(46, 46, 61);
-    height: 100%;
+    height: 100vh;
+    font-size: 1.1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
     .bubble {
         display: flex;
         align-items: flex-end;
+
+        color: white;
+        &.yourMess {
+            flex-direction: row-reverse;
+            .message {
+                border-bottom-right-radius: 0;
+            }
+        }
         img {
             width: 45px;
             border-radius: 50%;
             aspect-ratio: 1;
             object-fit: cover;
+            visibility: hidden;
+            &.displayPhoto {
+                visibility: visible;
+            }
         }
         .message {
-            color: white;
-            &.yourMess {
-                color: red;
-            }
+            background-color: rgb(255, 255, 255, 0.1);
+            max-width: 500px;
+            text-align: left;
+            margin: 5px 5px 0;
+            padding: 8px;
+            border-radius: 10px;
+            border-bottom-left-radius: 0;
         }
     }
 }
